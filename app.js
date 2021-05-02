a11b4a383e2d7e4af703cf338d3b999a254dc7f0
foxardt <lizenjulien@hotmail.com>
2021-05-02 22:17:01 +0200

    clean app.js file / add init.js

--- app.js
+++ app.js
@@ -1,41 +1,14 @@
-const fs = require('fs') 
-const { Client, Collection } = require('discord.js') 
+//imports
+const { Client } = require('discord.js')
+const {eventLoader, commandLoader} = require('./init.js') 
 require('dotenv').config() 
 
 const client = new Client() 
 
-client.commands = new Collection() 
-
-client.config = require('./config') 
-
 //Load events from event folder
-fs.readdir('./events/', (err, files) => {
-  if (err) return console.error 
-  files.forEach(file => {
-    if (!file.endsWith('.js')) return 
-    const evt = require(`./events/${file}`) 
-    let evtName = file.split('.')[0] 
-    console.log(`Loaded event '${evtName}'`) 
-    client.on(evtName, evt.bind(null, client)) 
-  }) 
-}) 
-
+eventLoader(client)
 //Load commands from commands folders
-fs.readdir('./commands/', async (err, folders) => {
-    if (err) return console.error 
-    folders.forEach(folder => {
-      fs.readdir(`./commands/${folder}/`, async (err, files) =>{
-        files.forEach(file => {
-          if (err) return console.error
-          if (!file.endsWith('.js')) return 
-          let props = require(`./commands/${folder}/${file}`) 
-          let cmdName = file.split('.')[0] 
-          console.log(`Loaded command '${cmdName}'`) 
-          client.commands.set(cmdName, props)
-        })
-      }) 
-    }) 
-  }) 
+commandLoader(client)
 
 //Log in using DISCORD_TOKEN from .env file
 client.login() 
---
+++ init.js
@@ -0,0 +1,36 @@
+//Load events from event folder
+exports.eventLoader = (client) => {
+  const fs = require('fs') 
+  fs.readdir('./events/', (err, files) => {
+    if (err) return console.error 
+    files.forEach(file => {
+      if (!file.endsWith('.js')) return 
+      const evt = require(`./events/${file}`) 
+      let evtName = file.split('.')[0] 
+      console.log(`Loaded event '${evtName}'`) 
+      client.on(evtName, evt.bind(null, client)) 
+    }) 
+  })
+}
+
+//Load commands from commands folders
+exports.commandLoader = (client) => {
+  const fs = require('fs')
+  const { Collection } = require('discord.js')
+  client.commands = new Collection()  
+  fs.readdir('./commands/', async (err, folders) => {
+    if (err) return console.error 
+    folders.forEach(folder => {
+      fs.readdir(`./commands/${folder}/`, async (err, files) =>{
+        files.forEach(file => {
+          if (err) return console.error
+          if (!file.endsWith('.js')) return 
+          let props = require(`./commands/${folder}/${file}`) 
+          let cmdName = file.split('.')[0] 
+          console.log(`Loaded command '${cmdName}'`) 
+          client.commands.set(cmdName, props)
+        })
+      }) 
+    }) 
+  }) 
+}
